be67f6ad0f4b7f0ab0aefbb8039af90f79570a7c
Rushitha Thipparapu <23110338@iitgn.ac.in>
2025-09-04 00:10:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+"""This program calculates total, average, pass/fail, and grade for students."""
+
 students = [
     {"name": "Rishi", "age": 20, "marks": [78, 85, 92, 67, 88]},
     {"name": "Hari", "age": 21, "marks": [6, 4, 1, 9, 19]}
@@ -15,22 +17,22 @@ for student in students:
     print("Average Marks:", average)
 
     if average >= 50:
-        print(student["name"], "has passed ")
+        print(student["name"], "has passed")
     else:
-        print(student["name"], "has failed ")
+        print(student["name"], "has failed")
 
     if average >= 90:
-        grade = "A+"
+        student_grade = "A+"
     elif average >= 75:
-        grade = "A"
+        student_grade = "A"
     elif average >= 60:
-        grade = "B"
+        student_grade = "B"
     elif average >= 50:
-        grade = "C"
+        student_grade = "C"
     else:
-        grade = "F"
+        student_grade = "F"
 
-    print("Grade:", grade)
+    print("Grade:", student_grade)
     print("-" * 30)
 
-print("Program finished ")
+print("Program finished")
